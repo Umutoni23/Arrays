20737df382d5d9408f6899529cddc8ef4b2b176a
Leoncie Umutoni <umutonileoncie37@gmail.com>
2026-02-17 14:57:13 +0200

    changes

---
+++ index.js
@@ -0,0 +1,15 @@
+// Exercise 1 : array initialization and Property Access
+const productCategories = ["", "Electronics", "Clothing", "Home & Kitchen", "Books", "Toys & Games"];
+// a
+console.log(productCategories[0]); 
+
+// b
+console.log(productCategories[productCategories.length - 1]);
+
+// c
+console.log(productCategories[productCategories.length - 1]);
+
+// d
+productCategories[1] = "pen";
+// e
+console.log(productCategories);
